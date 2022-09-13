fa7b5fd5186e650abe3360e61ea932f45b502c5c
John Oram <john@oram.ca>
2022-09-13 11:53:03 -0700

    cleanup, and get working clan script

--- parse_clans.py
+++ parse_clans.py
@@ -6,15 +6,8 @@ import pprint
 import random
 from typing import List, Dict, Optional
 
-clans = {}
 
-# Alex sort into clans
-# Spotcheck: jeannie/tom or betty/greg don't get Jody
-# Alex/John memorize the seed number once good
-
-
-
-class Family:
+class Person:
     name: str = ""
     shipping_address: str = ""
     email_address: str = ""
@@ -28,7 +21,7 @@ class Family:
         shipping_address: str = "",
         email_address: str = "",
         hints: str = "",
-        num_presents: int = 3,
+        num_presents: int = 1,
     ):
         self.name = name
         self.clan = clan
@@ -39,6 +32,12 @@ class Family:
         self.target_names = []
         self.receive_from_names = []
 
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
     @property
     def num_gifts_owed(self) -> int:
         return self.num_presents - len(self.receive_from_names)
@@ -47,111 +46,79 @@ class Family:
     def num_gifts_needed(self) -> int:
         return self.num_presents - len(self.target_names)
 
-    @property
-    def targets(self) -> List["Family"]:
-        for clan in clans.values():
-            for family in clan.families:
-                if family.name in self.target_names:
-                    yield family
+    def targets(self, people: List["Person"]) -> List["Person"]:
+        for person in people:
+            if person.name in self.target_names:
+                yield person
 
-class Clan:
+    def blacklist(self, people: List["Person"]) -> List["Person"]:
+        for person in people:
+            if person.clan == self.clan:
+                yield person
+            if person.name in self.receive_from_names:
+                yield person
 
-    def __init__(self, name):
-        self.name = name
-        self.families = []
-
-    @classmethod
-    def get(cls, name) -> "Clan":
-        if name not in clans:
-            clans[name] = Clan(name)
-        clans[name] = clans.get(name)
-        return clans[name]
-
-    def add_family(self, family: Family):
-        self.families.append(family)
-
-    @property
-    def num_gifts_owed(self) -> int:
-        return sum([family.num_gifts_owed for family in self.families])
-
-    @property
-    def num_gifts_needed(self) -> int:
-        return sum([family.num_gifts_needed for family in self.families])
-
-    def giver(self) -> Family:
-        elected_giver = None
-        for family in self.families:
-            if elected_giver is None or len(family.target_names) < len(elected_giver.target_names):
-                elected_giver = family
-        return elected_giver
-
-    def receiver(self, giver: Family) -> Optional[Family]:
-        elected_receiver = None
-        for family in self.families:
-            if elected_receiver is None:
-                elected_receiver = family
-                continue
-            if giver.name in family.receive_from_names:
-                continue
-            if len(family.receive_from_names) < len(elected_receiver.receive_from_names):
-                elected_receiver = family
-        if elected_receiver and elected_receiver.num_gifts_owed <= 0:
+    def get_another_target(self, people: List["Person"]) -> Optional["Person"]:
+        possible_receivers = [person for person in people if person not in list(self.blacklist(people))]  # not a target already
+        possible_receivers = [person for person in possible_receivers if person.num_gifts_owed > 0]  # not already full
+        if len(possible_receivers) == 0:
+            print("no valid receivers for", self)
             return None
-        return elected_receiver
+
+        target = random.choice(possible_receivers)
+        self.target_names.append(target.name)
+        target.receive_from_names.append(self.name)
+        return target
 
 
-def parse(filepath: str = "data.csv"):
-    global families
-    families = {}
-
-    # load from CSV
+def parse(filepath: str = "data.csv", presents: int = 1):
+    people = []
     with open(filepath) as f:
         data = csv.DictReader(f)
         for row in data:
-            name = row["Name"]
-            Clan.get(row["Clan"]).add_family(Family(
-                name=name,
-                shipping_address=row["Shipping Address"],
-                email_address=row["Email Address"],
-                hints=row["Message for Santa"],
-                num_presents=3,
+            people.append(Person(
+                name=row["Name"],
+                shipping_address=row["Mailing Address"],
+                email_address=row["Email"],
+                hints=row["Gift Suggestions/Ideas"],
+                num_presents=presents,
+                clan=row["Clan"],
             ))
+    return people
 
 
-def assign(seed=2303, num_targets=3, filename="blank_2021.csv"):
-    random.seed(seed)
-    parse(filename)
-    while any([clan.num_gifts_owed>0 for clan in clans.values()]):
-        clans_owing = [clan for clan in clans.values() if clan.num_gifts_needed > 0]
-        max_clan_owing = max([clan.num_gifts_needed for clan in clans_owing])
+def assign(seed, presents, filename):
+    global people
 
-        for clan in clans_owing:
-            if clan.num_gifts_needed != max_clan_owing:
-                continue
-            giver = clan.giver()
-            possible_receivers = [other_clan.receiver(giver) for other_clan in clans.values() if other_clan.name != clan.name]
-            possible_receivers = [receiver for receiver in possible_receivers if receiver is not None]
-            if len(possible_receivers) == 0:
-                continue
+    done = False
+    while not done:
+        seed += 1
+        random.seed(seed)
+        people = parse(filename, presents)
+        print(f"people {people}")
+        # assign targets
+        while not done:
+            people_owing_gifts = [person for person in people if person.num_gifts_needed > 0]
+            if len(people_owing_gifts) == 0:
+                done = True
+                break
 
-            receiver = random.choice(possible_receivers)
-
-            if giver.name in receiver.receive_from_names:
-                continue
-            if len(giver.target_names) > num_targets:
-                continue
-            giver.target_names.append(receiver.name)
-            receiver.receive_from_names.append(giver.name)
+            for giver in people_owing_gifts:
+                target = giver.get_another_target(people)
+                if target is None:
+                    print("no valid target for", giver)
+                    continue
+    return people
 
 
-assign(2304, 3, "./csvs/example_clan_2021.csv")
+if __name__ == "__main__":
+    people = assign(seed=1, presents=1, filename="./csvs/clans_2022.csv")
 
-# show results
-output = io.StringIO()
-writer = csv.writer(output, quoting=csv.QUOTE_NONNUMERIC)
-writer.writerow(["Giver", "Receiver", "Shipping Address", "Email Address", "Suggestions/Hints"])
-for clan in clans.values():
-    for family in clan.families:
-        for target in family.targets:
-            writer.writerow([family.name, target.name, target.shipping_address, target.email_address, target.hints])
-print(output.getvalue())
+    # show results
+    output = io.StringIO()
+    writer = csv.writer(output, quoting=csv.QUOTE_NONNUMERIC)
+    writer.writerow(["Giver", "Receiver", "Shipping Address", "Email Address", "Suggestions/Hints"])
+    for person in people:
+        for target in person.targets(people):
+            writer.writerow([person.name, target.name, target.shipping_address, target.email_address, target.hints])
+    print(output.getvalue())
